6638764f4cd275d3a91f427145ffba326b2057c7
NathalieZD <167153017+NathalieZD@users.noreply.github.com>
2024-05-31 20:28:38 -0600

    Tarea

--- firebase-get-to-know-flutter/step_02/lib/home_page.dart
+++ firebase-get-to-know-flutter/step_02/lib/home_page.dart
@@ -1,9 +1,11 @@
-// Copyright 2022 The Flutter Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
+import 'package:firebase_auth/firebase_auth.dart'
+    hide EmailAuthProvider, PhoneAuthProvider;
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import 'app_state.dart';
+import 'guest_book.dart'; // new
+import 'src/authentication.dart';
 import 'src/widgets.dart';
 
 class HomePage extends StatelessWidget {
@@ -21,6 +23,15 @@ class HomePage extends StatelessWidget {
           const SizedBox(height: 8),
           const IconAndDetail(Icons.calendar_today, 'October 30'),
           const IconAndDetail(Icons.location_city, 'San Francisco'),
+          // Add from here
+          Consumer<ApplicationState>(
+            builder: (context, appState, _) => AuthFunc(
+                loggedIn: appState.loggedIn,
+                signOut: () {
+                  FirebaseAuth.instance.signOut();
+                }),
+          ),
+          // to here
           const Divider(
             height: 8,
             thickness: 1,
@@ -31,6 +42,22 @@ class HomePage extends StatelessWidget {
           const Header("What we'll be doing"),
           const Paragraph(
             'Join us for a day full of Firebase Workshops and Pizza!',
+          ),
+          // Modify from here...
+          Consumer<ApplicationState>(
+  builder: (context, appState, _) => Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      if (appState.loggedIn) ...[
+        const Header('Discussion'),
+        GuestBook(
+          addMessage: (message) =>
+              appState.addMessageToGuestBook(message),
+          messages: appState.guestBookMessages, 
+                  ),
+                ],
+              ],
+            ),
           ),
         ],
       ),
